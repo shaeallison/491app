d06aac50886d8c1c864293e5afa5bf43c61dbac2
Shae <shae.allison7@gmail.com>
2014-09-16 09:55:59 -0400

    camera

---
+++ app.js
@@ -0,0 +1,13 @@
+function launchCamera(){
+navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.DATA_URL
+});}
+
+function onSuccess(imageData) {
+    var image = document.getElementById('myImage');
+    image.src = "data:image/jpeg;base64," + imageData;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
