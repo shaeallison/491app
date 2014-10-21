1594b2d0b39f5608da09cfbc85fa8858658aa644
Shae <shae.allison7@gmail.com>
2014-10-21 10:46:04 -0400

    parse

---
+++ ParseStarterProject/ParseStarterProject/ParseStarterProjectViewController.h
@@ -0,0 +1,12 @@
+//
+//  ParseStarterProjectViewController.h
+//  ParseStarterProject
+//
+//  Copyright 2014 Parse, Inc. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface ParseStarterProjectViewController : UIViewController
+
+@end
