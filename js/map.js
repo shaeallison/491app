fa4996273fddd10b120713f8aa311bcc005f3727
Shae <shae.allison7@gmail.com>
2014-11-11 09:48:50 -0500

    panoramio

--- js/map.js
+++ js/map.js
@@ -95,11 +95,26 @@ function generateMap(position) {
     var map = new google.maps.Map(document.getElementById("map-canvas"),
         mapOptions);
     
-    var image = 'data';
-        marker = new google.maps.Marker({
+    var panoramioLayer = new google.maps.panoramio.PanoramioLayer();
+    panoramioLayer.setMap(map);
+
+    var photoPanel = document.getElementById('photo-panel');
+    map.controls[google.maps.ControlPosition.RIGHT_TOP].push(photoPanel);
+
+    google.maps.event.addListener(panoramioLayer, 'click', function(photo) {
+    var li = document.createElement('li');
+    var link = document.createElement('a');
+    link.innerHTML = photo.featureDetails.title + ': ' +
+        photo.featureDetails.author;
+    link.setAttribute('href', photo.featureDetails.url);
+    li.appendChild(link);
+    photoPanel.appendChild(li);
+    photoPanel.style.display = 'block';
+    
+    var marker = new google.maps.Marker({
         position: currentLatLng,
         map: map,
-        icon: image
+        title: 'CurrentLocation'
     });
 
 }
