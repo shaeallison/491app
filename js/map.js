65d0a2cf6d1ca68a23dfe7a5b55d6e56c6f5d123
Shae <shae.allison7@gmail.com>
2014-11-11 10:36:03 -0500

    current location

--- js/map.js
+++ js/map.js
@@ -1,5 +1,4 @@
-
-//map index 
+//map profile 
 
 function onError(error) {
     console.log("onError()");
@@ -8,7 +7,7 @@ function onError(error) {
 }
 
 function generateMap(position) {
-    console.log("generateMap()");
+    console.log("generateMap2()");
     console.log(position.coords.latitude);
     console.log(position.coords.longitude);
     var currentLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
@@ -17,112 +16,22 @@ function generateMap(position) {
         //center:new google.maps.LatLng(-34.397, 150.644),
         center: currentLatLng,
         zoom: 15,
-        styles: [
-    {
-        "featureType": "landscape.natural",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "color": "#e0efef"
-            }
-        ]
-    },
-    {
-        "featureType": "poi",
-        "elementType": "geometry.fill",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "hue": "#1900ff"
-            },
-            {
-                "color": "#c0e8e8"
-            }
-        ]
-    },
-    {
-        "featureType": "landscape.man_made",
-        "elementType": "geometry.fill"
-    },
-    {
-        "featureType": "road",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "lightness": 100
-            },
-            {
-                "visibility": "simplified"
-            }
-        ]
-    },
-    {
-        "featureType": "road",
-        "elementType": "labels",
-        "stylers": [
-            {
-                "visibility": "off"
-            }
-        ]
-    },
-    {
-        "featureType": "water",
-        "stylers": [
-            {
-                "color": "#7dcdcd"
-            }
-        ]
-    },
-    {
-        "featureType": "transit.line",
-        "elementType": "geometry",
-        "stylers": [
-            {
-                "visibility": "on"
-            },
-            {
-                "lightness": 700
-            }
-        ]
-    }
-]
+
     };
     var map = new google.maps.Map(document.getElementById("map-canvas"),
         mapOptions);
     
-    var panoramioLayer = new google.maps.panoramio.PanoramioLayer();
-    panoramioLayer.setMap(map);
-
-    var photoPanel = document.getElementById('photo-panel');
-    map.controls[google.maps.ControlPosition.RIGHT_TOP].push(photoPanel);
-
-    google.maps.event.addListener(panoramioLayer, 'click', function(photo) {
-    var li = document.createElement('li');
-    var link = document.createElement('a');
-    link.innerHTML = photo.featureDetails.title + ': ' +
-        photo.featureDetails.author;
-    link.setAttribute('href', photo.featureDetails.url);
-    li.appendChild(link);
-    photoPanel.appendChild(li);
-    photoPanel.style.display = 'block';
-    
     var marker = new google.maps.Marker({
         position: currentLatLng,
         map: map,
-        title: 'CurrentLocation'
+        title: 'Current Location'
     });
-
 }
 
 
 
     
-/*For Browser testing. Comment out for app testing.*/
+/*For Browser testing. Comment out for app testing. */
 
     var position ={
         coords:{latitude:35.913200, longitude:-79.055845}
@@ -130,9 +39,7 @@ function generateMap(position) {
     console.log(position.coords.latitude);
     
     google.maps.event.addDomListener(window, 'load' ,generateMap(position));
-
-
-
+    
 
 
 
