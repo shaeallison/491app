e7fc0e558dcbc00bdc5fe787a38447049e97ffe1
Shae <shae.allison7@gmail.com>
2014-11-18 00:58:41 -0500

    parse

---
+++ apptest.js
@@ -0,0 +1,119 @@
+Parse.initialize("lgAdW3MFp6eMnSdtkcYKXqsjwLDVPvzPWYYZa9V3", "IpsgowMm56E1TVYVvF3UC1x9gA6I8Lka4MId9HiP");
+CommentObject = Parse.Object.extend("CommentObject");
+
+var htmlBuilder = "";
+
+var currentLocation;
+
+
+$(document).ready(function() {
+        
+	
+	getList(CommentObject);
+	
+	
+	if($("#sumbitEventBtn").length === 1) {
+		currentLocation=null;
+		navigator.geolocation.getCurrentPosition(function(pos) {
+			//store the long/lat
+			currentLocation = {longitude:pos.coords.longitude, latitude:pos.coords.latitude};
+			$("#sumbitEventBtn").removeAttr("disabled");
+		}, function(err) {
+			//Since geolocation failed, we can't allow the user to submit
+			alert("Sorry, but we couldn't find your location.");
+		});
+
+    }
+    
+    $("#commentForm").on("submit", function(e) {
+	e.preventDefault();
+ 
+	//get values
+	var name = $("#name").val();
+	var venue = $("#venue").val();
+	var town = $("#town").val();
+	var state = $("#state").val();
+        var day = $("#day").val();
+	var time = $("#time").val();
+	var cost = $("#cost").val();
+        var description=$("#description").val();
+ 
+	var comment = new CommentObject();
+	var point = new Parse.GeoPoint({latitude: currentLocation.latitude, longitude: currentLocation.longitude});
+	comment.save(
+			{
+				name:name,
+				venue:venue,
+				town:town,
+				state:state,
+				day:day,
+				time:time,
+				cost:cost,
+				description:description,
+				location:point
+			},{
+				success:function(object) {
+					console.log("Saved object");
+					alert("You've created an event");
+				},
+				error:function(model, error) {
+					console.log(":(");
+				}
+			});
+ 
+	});
+	
+	
+	if($("#event").length === 1) {
+
+		//Update status 
+		$("#event").html("Loading Content...");
+
+		navigator.geolocation.getCurrentPosition(function(pos) {
+			var myLocation = new Parse.GeoPoint({latitude: pos.coords.latitude, longitude: pos.coords.longitude});
+
+			//Begin our query
+			var query = new Parse.Query(CommentObject);
+			//Only within 10 miles
+			query.withinMiles("location", myLocation, 10);
+			//only within last week
+			var lastWeek = new Date();
+			lastWeek.setDate(lastWeek.getDate()-7);
+			query.greaterThan("createdAt", lastWeek);
+			query.find({
+				success:function(results) { getList(results,myLocation); },
+				error: function(error) { alert("Error: " + error.code + " " + error.message); }
+			});
+
+		}, function(err) {
+			//Since geolocation failed, we can't allow the user to submit
+			alert("Sorry, we couldn't find your location.");
+		},{timeout:20000,enableHighAccuracy:false});
+    }
+});
+
+
+
+
+function getList(CommentObject){
+    console.log("getList" + CommentObject);
+    var query = new Parse.Query(CommentObject);
+    query.find({
+        success: function(results) {
+            console.log(results);
+            $.each(results, function( index, value ) {
+            console.log(results[index].attributes.cost);
+            htmlBuilder +=  '<div class="box">' + '<div class="row">' + '<div class="small-9 columns">' + '<ul>' + results[index].attributes.name + '</br>' + results[index].attributes.venue + " : " + results[index].attributes.town + ", " + results[index].attributes.state +  '</br>' + results[index].attributes.day + " | " + results[index].attributes.time + '</br>'
+            + results[index].attributes.cost + '</ul>' + '</div>' + '<div class="small-2 columns">'+ '</div>' +'<div class="small-2 columns">'+'<button id="element1" onclick="javascript:changeText(1)">Not Going</button>' + '</br>' + '<div class="friend-box">' + '<i class="fi-torso">' + " 3" + '</i>' + '</div>' + '</div>' + '</div>' + '</div>' + '</a>';
+});
+            $("#event").html(htmlBuilder);
+        },
+        
+        error: function(error) {
+        }
+	
+	
+        
+    });
+	
+}
