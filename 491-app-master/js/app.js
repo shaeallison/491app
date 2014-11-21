4472572d2b9b2b2a37d7e45f0dde198f17ac3ccb
Shae <shae.allison7@gmail.com>
2014-11-20 19:10:03 -0500

    capture photo js

--- 491-app-master/js/app.js
+++ 491-app-master/js/app.js
@@ -194,16 +194,29 @@ function errorGeo(error){
 		}
 	}
 	
-	function capturePhoto(){
-        //alert("capture button working");
-    navigator.camera.getPicture(gotPic,failHandler,{quality:25, destinationType:0 });
-}   
+function capturePhoto() {
+    console.log("capturePhoto()");
+    navigator.camera.getPicture(uploadPhoto,null,{sourceType:2,quality:60,});
+}
 
-function choosePhoto(){
-        //alert("capture button working");
-    navigator.camera.getPicture(gotPic,failHandler,{sourceType:0, destinationType:0, quality:25});
-}  
- 
+
+
+function uploadPhoto(data){
+    //send file to server
+    console.log("uploadPhoto()");
+   
+   cameraPic.src = data;
+        navigator.notification.alert(
+            'Your photo has been uploaded', //message
+            okay,                           //callback
+            'Photo uploaded',               //title
+            'OK'                            //buttonName
+        );
+}
+
+function okay (){
+        //Do Something
+}
 	
 	function gotPic(data) {
 		console.log('got here');
